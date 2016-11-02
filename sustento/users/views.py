fc0410b3519fe23061347378eda63ac6fc5b0d21
Mark Vella <markvella@Marks-MacBook.fios-router.home>
2016-11-02 00:28:11 -0400

    Experimenting

--- sustento/users/views.py
+++ sustento/users/views.py
@@ -88,10 +88,9 @@ def UserReceive(request):
     # if this is a POST request from Twilio, we need to process the POST data
     if request.method == 'POST':
         #receive the Twilio post data and create a new response object
-        respPhone = "phone" #request.POST.get('From')
+        respPhone = request.POST.get('From')[-10:]
         respMessage = request.POST.get('Body')
-        #try request.META["HTTP_HOST"]
-        resp = Response(phone=respPhone, anonymous=False, message=respMessage)
+        resp = Response(phone=respPhone, anonymous=False, message=request.META["HTTP_HOST"])
         resp.save()
         return HttpResponseRedirect('/users/~send/')
     # if a GET or wrong domain, we'll just redirect
