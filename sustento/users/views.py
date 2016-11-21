72bf913eb6aabf5b5862e3021d2871b493cc84c4
Mark Vella <markvella@Marks-MacBook.local>
2016-11-21 09:40:09 -0500

    Made it stop texting me

--- sustento/users/views.py
+++ sustento/users/views.py
@@ -245,8 +245,8 @@ def sendUserMessage(message, user):
 
 @csrf_exempt
 def UserSchedule(request):
-    user = User.objects.get(phone="2035601401")
-    sendUserMessage(datetime.datetime.strftime(datetime.datetime.now(), "%c"), user)
+    #user = User.objects.get(phone="2035601401")
+    #sendUserMessage(datetime.datetime.strftime(datetime.datetime.now(), "%c"), user)
     return HttpResponse('<?xml version="1.0" encoding="UTF-8"?><Response></Response>')
 
 #import schedule
