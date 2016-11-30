d7f84516034eb557f33b306c97635b3fd9238d20
Mark Vella <markvella@Marks-MacBook.local>
2016-11-30 13:47:48 -0500

    date picker

--- sustento/users/forms.py
+++ sustento/users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+import datetime
 
 class UserSendForm(forms.Form):
 #    def __init__(self, *args, **kwargs):
@@ -10,6 +11,13 @@ class UserSendForm(forms.Form):
 class JournalEntryForm(forms.Form):
     text = forms.CharField(label='Journal Entry', max_length=1998)
 
-class RemindersForm(forms.Form):
-    when = forms.DateTimeField(label='Weekly Reminder Date and Time')
-    text = forms.CharField(label='Reminder Text', max_length=150)
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class RemindersForm(forms.ModelForm):
+    class Meta:
+        model = Reminders
+        fields = ['when', 'text']
+        widgets = {
+            'whehn': DateInput(),
+        }
