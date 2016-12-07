acad1829eb63cd32dbc62e2aa387cd816294c058
Mark Vella <markvella@Marks-MacBook.local>
2016-12-07 13:53:00 -0500

    Trying to get static urls to work

--- config/wsgi.py
+++ config/wsgi.py
@@ -16,6 +16,7 @@ framework.
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 #if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
 #    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
@@ -29,6 +30,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 #if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
 #    application = Sentry(application)
 # Apply WSGI middleware here.
