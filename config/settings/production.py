e6b5e806b43445586c3c5124f8ff99fa72974a48
Mark Vella <markvella@Marks-MacBook.local>
2016-12-07 14:30:17 -0500

    Added staticfiles

--- config/settings/production.py
+++ config/settings/production.py
@@ -231,15 +231,3 @@ ADMIN_URL = env('DJANGO_ADMIN_URL')
 TWILIO_ACCOUNT_SID = env('TWILIO_ACCOUNT_SID')
 TWILIO_API_AUTH = env('TWILIO_API_AUTH')
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = ROOT_DIR.path('staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    APPS_DIR.path('static'),
-)
-
