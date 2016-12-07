55e69511dd8f4ba27445cd6d8b368fbfa9e35703
Mark Vella <markvella@Marks-MacBook.local>
2016-12-07 14:26:30 -0500

    Added staticfiles

--- config/settings/production.py
+++ config/settings/production.py
@@ -235,11 +235,11 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles')
+STATIC_ROOT = ROOT_DIR.path('staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(APPS_DIR, 'static'),
+    APPS_DIR.path('static'),
 )
 
