a0e31ab22637bd81c4b07060b5b68cc67df6d3df
Mark Vella <markvella@Marks-MacBook.local>
2016-12-07 13:39:31 -0500

    Trying to get static urls to work

--- config/settings/common.py
+++ config/settings/common.py
@@ -178,7 +178,7 @@ STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
-    str(APPS_DIR.path('static')),
+    str(ROOT_DIR.path('static')),
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
